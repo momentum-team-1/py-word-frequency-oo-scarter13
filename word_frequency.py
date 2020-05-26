9c0111da865a275d2c1528082109aa7abaf8af98
scarter13 <scarter13@gmail.com>
2020-05-26 19:20:54 -0400

    working on passing variables between classes

--- word_frequency.py
+++ word_frequency.py
@@ -3,23 +3,39 @@ STOP_WORDS = [
     'he', 'i', 'in', 'is', 'it', 'its', 'of', 'on', 'that', 'the', 'to',
     'were', 'will', 'with'
 ]
-
+import string
+punctuation = string.punctuation
+word_counts = {}
 
 class FileReader:
     def __init__(self, filename):
-        pass
+        self.filename = filename
+
+
 
     def read_contents(self):
         """
         This should read all the contents of the file
         and return them as one string.
         """
-        raise NotImplementedError("FileReader.read_contents")
+        opened_file = open(self.filename)
+        self.opened_file = opened_file
+        read_contents = (self.opened_file.read())
+        self.read_contents = read_contents
+        opened_file.close()
+        #print (self.read_contents)
+        return self.read_contents
 
 
 class WordList:
     def __init__(self, text):
-        pass
+        self.text = text
+        #print (self.text)
+        #print ("was printed from inside the wordlist init")   
+        #return self.text 
+        """
+        Apparently, return self.text is not necessary.  Discuss?  I'll offer up a possiblity.  The .text is an attribute of the object now.  It can be called from the following method without being passed in because it relates specifically to the "self" to which the next method will refer.  Is that right?  Or, is that maybe partly right?  Perhaps it will pass freely because it is still within the scope of the class WordList.  I say this because I noticed that the auto-fill wasn't working on variables anymore once I got down into the Printer class.  
+        """
 
     def extract_words(self):
         """
@@ -27,14 +43,34 @@ class WordList:
         is responsible for lowercasing all words and stripping
         them of punctuation.
         """
-        raise NotImplementedError("WordList.extract_words")
+        lower_case_text = (self.text.lower())
+        #print (lower_case_text)
+        no_hyphen = lower_case_text.replace("-", " ")
+        #print (no_hyphen)
+        no_punctuation = ""
+        for char in no_hyphen:
+            if char not in punctuation:
+                no_punctuation = no_punctuation + char
+        stripped_text = no_punctuation.strip()
+        all_words = stripped_text.split()
+        #print (all_words)
+        #print ("printed from extract words post-split")
+        self.all_words = all_words
+             
 
     def remove_stop_words(self):
         """
         Removes all stop words from our word list. Expected to
         be run after extract_words.
         """
-        raise NotImplementedError("WordList.remove_stop_words")
+        #print (self.all_words)
+        no_stop_words = []
+        for word in self.all_words:
+            if word not in STOP_WORDS:
+                no_stop_words.append(word)
+        #print (no_stop_words)
+        self.no_stop_words = no_stop_words
+
 
     def get_freqs(self):
         """
@@ -43,12 +79,29 @@ class WordList:
         extract_words and remove_stop_words. The data structure
         could be a dictionary or another type of object.
         """
-        raise NotImplementedError("WordList.get_freqs")
+        longest_word = 0
+        for word in self.no_stop_words:
+            if len(word) > longest_word:
+                longest_word = len(word)
+        self.longest = longest_word
+        for word in self.no_stop_words:
+            if word in word_counts:
+                word_counts[word] += 1
+            else:
+                word_counts[word] = 1
+        self.word_freqs_descending = (sorted(word_counts.items(), key=lambda seq: seq[1], reverse=True))
+        print (self.word_freqs_descending)
+        print (f" the longest word is {self.longest} characters long.")
+        return self.word_freqs_descending, self.longest
 
 
 class FreqPrinter:
-    def __init__(self, freqs):
-        pass
+    def __init__(self, freqs, longest):
+        self.freqs = freqs 
+        self.longest = longest
+        print (self.longest)
+        print ("printed inside the printer class")
+
 
     def print_freqs(self):
         """
@@ -67,7 +120,9 @@ class FreqPrinter:
        rights | 6    ******
         right | 6    ******
         """
-        raise NotImplementedError("FreqPrinter.print_freqs")
+
+
+
 
 
 if __name__ == "__main__":
